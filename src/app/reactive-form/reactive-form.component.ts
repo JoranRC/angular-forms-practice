f6cf31fbe75d8de98830f7f85fc0aa28e22e1c76
JoranRC <33453681+JoranRC@users.noreply.github.com>
2022-03-06 21:53:37 +0100

    Formating of reactive form component

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ValidatorFn,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'reactive-form-ui',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.scss']
+  styleUrls: ['./reactive-form.component.scss'],
 })
 export class ReactiveFormComponent implements OnInit {
   reactiveForm: FormGroup;
   firstName: FormControl;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     //by hand creation of the reactiveForm without FormBuilder
@@ -19,45 +26,45 @@ export class ReactiveFormComponent implements OnInit {
 
     //using the formBuilder service to create the formGroup
     this.reactiveForm = this.fb.group({
-        firstName: ['', [Validators.required, Validators.minLength(3)]],
-        customValidationInput: ['GOAT', this.customValidation()]
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      customValidationInput: ['GOAT', this.customValidation()],
     });
-
-    
   }
 
   populateForm(): void {
-     //use setValue to set all the formcontrols, use patchValue if you want to set the values of a subset of the formcontrols
+    //use setValue to set all the formcontrols, use patchValue if you want to set the values of a subset of the formcontrols
     this.reactiveForm.patchValue({
-        firstName: 'John'
+      firstName: 'John',
     });
   }
 
   clearValidation(): void {
-      this.reactiveForm.get('firstName').clearValidators();
-      this.reactiveForm.get('firstName').updateValueAndValidity();
+    this.reactiveForm.get('firstName').clearValidators();
+    this.reactiveForm.get('firstName').updateValueAndValidity();
   }
 
   setValidation(): void {
-      this.reactiveForm.get('firstName').setValidators([Validators.required, Validators.minLength(3)]);
-      this.reactiveForm.get('firstName').updateValueAndValidity();
+    this.reactiveForm
+      .get('firstName')
+      .setValidators([Validators.required, Validators.minLength(3)]);
+    this.reactiveForm.get('firstName').updateValueAndValidity();
   }
 
   customValidation(): ValidatorFn {
-    return (control: AbstractControl): {[key: string]: boolean} | null => {
-        if(control.value !== 'GOAT') {
-            return {
-                'customValidationInput': true
-            }
-        }
-    }
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      if (control.value !== 'GOAT') {
+        return {
+          customValidationInput: true,
+        };
+      }
+    };
   }
 
   addValueChangesSubscribtion(): void {
     //subscribing to valueChanges
-    this.reactiveForm.get('firstName').valueChanges.subscribe(
-        value => console.log('firstName value changed to: ', value)
-    );
+    this.reactiveForm.get('firstName').valueChanges.subscribe((value) => {
+      console.log('firstName value changed to: ', value);
+    });
   }
 
   logForm(): void {
@@ -68,5 +75,4 @@ export class ReactiveFormComponent implements OnInit {
     console.log('Submit in reactive form is called');
     console.log(this.reactiveForm);
   }
-
 }
