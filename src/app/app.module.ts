05862759f5f5150144324f479ad95f6e42b73c46
JoranRC <33453681+JoranRC@users.noreply.github.com>
2022-03-14 22:02:44 +0100

    Removed welcome component as it is no longer used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { WelcomeComponent } from './welcome/welcome.component';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
@@ -16,7 +15,6 @@ import { CustomCheckboxesFormComponent } from './custom-checkboxes-form/custom-c
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
     TemplateDrivenComponent,
     ReactiveFormComponent,
     FormArrayComponent,
